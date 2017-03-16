849e45de38df1deffffc9912b4f4898456912996
Matt <matt@objectlabs.com>
2017-03-16 15:26:53 -0700

    Adding exported tests as

---
+++ test/index.js
@@ -0,0 +1,17 @@
+var o = require('@carbon-io/atom').o(module).main
+var _o = require('@carbon-io/bond')._o(module)
+var testtube = require('@carbon-io/test-tube')
+
+var tests = o({
+  _type: testtube.Test,
+  name: 'CarbonCoreTests',
+  tests: [
+    require('@carbon-io/fibers').$Test, 
+    require('@carbon-io/bond').$Test,
+    require('@carbon-io/atom').$Test,
+    require('@carbon-io/test-tube').$Test,
+    require('@carbon-io/leafnode').$Test
+  ]
+})
+
+module.exports = tests
