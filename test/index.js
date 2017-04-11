330c6c587ef8f1603cda29c9fc2d2f342e62b66e
Greg Banks <gbanks@gmail.com>
2017-04-10 19:40:44 -0700

    test must be wrapped in __

--- test/index.js
+++ test/index.js
@@ -1,17 +1,18 @@
+var __ = require('@carbon-io/fibers').__(module).main
 var o = require('@carbon-io/atom').o(module).main
 var _o = require('@carbon-io/bond')._o(module)
 var testtube = require('@carbon-io/test-tube')
 
-var tests = o({
-  _type: testtube.Test,
-  name: 'CarbonCoreTests',
-  tests: [
-    require('@carbon-io/fibers').$Test, 
-    require('@carbon-io/bond').$Test,
-    require('@carbon-io/atom').$Test,
-    require('@carbon-io/test-tube').$Test,
-    require('@carbon-io/leafnode').$Test
-  ]
+__(function() {
+  module.exports = o({
+    _type: testtube.Test,
+    name: 'CarbonCoreTests',
+    tests: [
+      require('@carbon-io/fibers').$Test,
+      require('@carbon-io/bond').$Test,
+      require('@carbon-io/atom').$Test,
+      require('@carbon-io/test-tube').$Test,
+      require('@carbon-io/leafnode').$Test
+    ]
+  })
 })
-
-module.exports = tests
